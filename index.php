f549d9c1295a48e6d196e7bca890b90a10317ff2
i-Payments <108110843+iPayments@users.noreply.github.com>
2022-10-24 12:59:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-include'./iPaymentsLib';
-
+include'./iPaymentsLib/config.php';
+include'./iPaymentsLib/lib.php';
+//Your Other Code
+$order_ID = createOrder("2",$accepted_currency,$callback,$site_orderSuccess_link,$private_key,$public_key);
+$new_order = "INSERT INTO `ips_tbl_orders`(`order_id`, `status`, `order_for`, `amount`) VALUES ('$order_ID','Pending','cloudMini','2')";
+if(mysqli_query($connection, $new_order)){
+header("Location: https://secure-pay.i-payments.site/?orderID=$order_ID");
+}
+?>
