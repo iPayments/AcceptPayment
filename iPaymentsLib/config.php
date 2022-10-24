d0215fd6eef969c693192bfb1acfe4cb2b35b385
i-Payments <108110843+iPayments@users.noreply.github.com>
2022-10-24 13:04:05 +0530

    Update config.php

--- iPaymentsLib/config.php
+++ iPaymentsLib/config.php
@@ -1,29 +1,18 @@
 <?php
-
-$site_link = "";
-
-$host = "";
-
-$dbPass = "";
-
-$dbName = "";
-
-$dbuser = "";
-
-$private_key = "";
-
-$public_key = "";
-
-$accepted_currency = "TRX";
-
-$callback = "redirect";
-
+$site_orderSuccess_link = "https://example.com/success.php"; //Site Success Page
+$host = "localhost"; //Host
+$dbPass = "pass"; //DB Pass
+$dbName = "name"; //DB Name
+$dbuser = "user"; //DB UserName
+$private_key = "key1"; //iPayments Developer Private Key
+$public_key = "key2"; //iPayments Developer Public Key
+$accepted_currency = "TRX"; //Payment Accepted Currencys
+$callback = "redirect"; //Always Redirect
+$connection = new mysqli($host, $dbuser, $dbPass, $dbName);
+// Check connection
+if ($connection->connect_error) {
+die("Connection failed: " . $connection->connect_error);
+}else{
+//Nothing;
+}
 ?>
-
-
-
-
-
-
-
-
