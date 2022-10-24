10be06e6bb70c2d598949ccb77f2e4e47d82da53
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2022-10-24 12:37:57 +0530

    Update config.php

--- iPaymentsLib/config.php
+++ iPaymentsLib/config.php
@@ -1,9 +1,29 @@
 <?php
+
+$site_link = "";
+
 $host = "";
+
 $dbPass = "";
+
 $dbName = "";
+
 $dbuser = "";
+
 $private_key = "";
+
 $public_key = "";
+
 $accepted_currency = "TRX";
+
+$callback = "redirect";
+
 ?>
+
+
+
+
+
+
+
+
